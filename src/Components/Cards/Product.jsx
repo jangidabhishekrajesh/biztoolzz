abbb23206719bfccc69360a13ea08d67168d22bc
jangidabhishekrajesh <83941830+jangidabhishekrajesh@users.noreply.github.com>
2021-09-16 11:20:41 +0530

    noe add module

--- src/Components/Cards/Product.jsx
+++ src/Components/Cards/Product.jsx
@@ -20,7 +20,7 @@ let Product = () => {
                     <div className="col-md-6 col-auto p-5 my-1">
                         <h4 className="text-center mb-2">WEB BUILDER</h4>
                         <a href="/login"><img src={webbd} style={{width:"100%"}} height="260" alt="" /></a>
-                        <a className="productLink" href="https://builder.biztoolzz.com" target="_blank"><h6 className="text-center mt-2">BUILD BEAUTIFUL WEBSITES</h6></a>
+                        <a className="productLink" href="https://builder.biztoolzz.com" target="_blank" rel="noreferrer"><h6 className="text-center mt-2">BUILD BEAUTIFUL WEBSITES</h6></a>
                     </div>
                 </Fade>
                 <Fade right cascade>
@@ -34,21 +34,21 @@ let Product = () => {
                     <div className="col-md-6 col-auto p-5 my-1">
                         <h4 className="text-center mb-2">BIZZLINC/ TREE</h4>
                         <a href="/login"><img src={bizz} style={{width:"100%"}} height="260" alt="" /></a>
-                        <a className="productLink" href="http://bizzlinc.com/" target="_blank"><h6 className="text-center mt-2">MANAGE YOUR BUSINESS LINKS</h6></a>
+                        <a className="productLink" href="http://bizzlinc.com/" target="_blank" rel="noreferrer"><h6 className="text-center mt-2">MANAGE YOUR BUSINESS LINKS</h6></a>
                     </div>
                 </Fade>
                 <Fade right cascade>
                     <div className="col-md-6 col-auto p-5 my-1">
                         <h4 className="text-center mb-2">PROJECT MANAGEMENT</h4>
                         <a href="/login"><img src={project} style={{width:"100%"}} height="260" alt="" /></a>
-                       <a className="productLink" href="https://pm.biztoolzz.com/public/" target="_blank"><h6 className="text-center mt-2">KEEP TRACK OF YOUR PROJECTS</h6></a>
+                       <a className="productLink" href="https://pm.biztoolzz.com/public/" target="_blank" rel="noreferrer"><h6 className="text-center mt-2">KEEP TRACK OF YOUR PROJECTS</h6></a>
                     </div>
                 </Fade>
                 <Fade left cascade>
                     <div className="col-md-6 col-auto p-5 my-1">
                         <h4 className="text-center mb-2">QR CODE BUILDER</h4>
                         <a href="/login"><img src={QR} style={{width:"100%"}} height="260" alt="" /></a>
-                        <a className="productLink" href="https://qr.biztoolzz.com/" target="_blank"><h6 className="text-center mt-2">SHARE YOUR BUSINESS INFORMATION QUICKLY AND EASILY</h6></a>
+                        <a className="productLink" href="https://qr.biztoolzz.com/" target="_blank" rel="noreferrer"><h6 className="text-center mt-2">SHARE YOUR BUSINESS INFORMATION QUICKLY AND EASILY</h6></a>
                     </div>
                 </Fade>
                 <Fade right cascade>
@@ -69,14 +69,14 @@ let Product = () => {
                     <div className="col-md-6 col-auto p-5 my-1">
                         <h4 className="text-center mb-2">RESUME BUILDER</h4>
                         <a href="/login"><img src={resume} style={{width:"100%"}} height="260" alt="" /></a>
-                        <a className="productLink" href="https://resume.biztoolzz.com/" target="_blank"><h6 className="text-center mt-2">CREATE PROFESSIONAL RESUMES FAST AND FREE</h6></a>
+                        <a className="productLink" href="https://resume.biztoolzz.com/" target="_blank" rel="noreferrer"><h6 className="text-center mt-2">CREATE PROFESSIONAL RESUMES FAST AND FREE</h6></a>
                     </div>
                 </Fade> 
                 <Fade left cascade>
                     <div className="col-md-6 col-auto p-5 my-1">
                         <h4 className="text-center mb-2">HR MANAGEMENT</h4>
                         <a href="/login"><img src={hr} style={{width:"100%"}} height="260" alt="" /></a>
-                        <a className="productLink" href="https://hrm.biztoolzz.com/public/" target="_blank"><h6 className="text-center mt-2">
+                        <a className="productLink" href="https://hrm.biztoolzz.com/public/" target="_blank" rel="noreferrer"><h6 className="text-center mt-2">
                         MANAGE YOUR EMPLOYEES WITH ATTENDANCE TRACKER, STAFF TRACKER AND MORE</h6></a>
                     </div>
                 </Fade>
@@ -84,7 +84,7 @@ let Product = () => {
                     <div className="col-md-6 col-auto p-5 my-1">
                         <h4 className="text-center mb-2">MARKETING TOOL</h4>
                         <a href="/login"><img src={market} style={{width:"100%"}} height="260" alt=""/></a>
-                        <a className="productLink" href="https://marketing.biztoolzz.com/" target="_blank"><h6 className="text-center mt-2">
+                        <a className="productLink" href="https://marketing.biztoolzz.com/" target="_blank" rel="noreferrer"><h6 className="text-center mt-2">
                         GET ACCESS TO 1000+ MARKETING CREATIVES, FLYER IDEAS AND USEFUL LINKS</h6></a>
                     </div>
                 </Fade>
